69576588a081689d89a034d6b4123629792673f6
lijunyi <platotomato2002@gmail.com>
2025-05-19 18:56:24 +0800

    feat: 层级结构

---
+++ grow-interfaces/src/main/java/github/grit/grow/GrowApplication.java
@@ -0,0 +1,15 @@
+package github.grit.grow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class GrowApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GrowApplication.class, args);
+    }
+} 
