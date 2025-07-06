aa1a941cc083fc0f14c6b6ede11ac3d21381a5ac
Moncoder <ljy200220192020@outlook.com>
2025-07-06 22:25:23 +0800

    feat: 生成租户相关实体类

--- gaia-infrastructure/src/main/java/github/grit/gaia/infrastructure/persistence/repository/impl/GaiaTenantRepositoryImpl.java
+++ gaia-infrastructure/src/main/java/github/grit/gaia/infrastructure/persistence/repository/impl/GaiaTenantRepositoryImpl.java
@@ -1,11 +1,11 @@
 package github.grit.gaia.infrastructure.persistence.repository.impl;
 
+import com.baomidou.mybatisplus.extension.service.IService;
+import github.grit.gaia.domain.repository.IGaiaTenantRepository;
 import github.grit.gaia.infrastructure.persistence.entity.GaiaTenant;
 import github.grit.gaia.infrastructure.persistence.mapper.GaiaTenantMapper;
-import github.grit.gaia.infrastructure.persistence.repository.IGaiaTenantRepository;
 import org.springframework.stereotype.Repository;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import github.grit.gaia.infrastructure.persistence.entity.GaiaAccountIntegrate;
 
 
 /**
