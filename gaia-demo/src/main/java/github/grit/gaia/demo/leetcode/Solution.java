0f3b589340b841a9a99977d326e6fc1f63669458
Moncoder <ljy200220192020@outlook.com>
2025-09-27 23:18:03 +0800

    feat: leetcode3689

--- gaia-demo/src/main/java/github/grit/gaia/demo/leetcode/Solution.java
+++ gaia-demo/src/main/java/github/grit/gaia/demo/leetcode/Solution.java
@@ -21,6 +21,17 @@ public class Solution {
         return res;
     }
 
+    // 3689. 最大子数组总值 I
+    public long maxTotalValue(int[] nums, int k) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[i]);
+        }
+        return (long) (max - min) * k;
+    }
+
     public static void main(String[] args) {
 
     }
