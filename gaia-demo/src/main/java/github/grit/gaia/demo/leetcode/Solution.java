e78ced6b4244b4e5b30d7af5b1c13d101f6f45ab
Moncoder <ljy200220192020@outlook.com>
2025-09-27 22:48:33 +0800

    feat: leetcode812

--- gaia-demo/src/main/java/github/grit/gaia/demo/leetcode/Solution.java
+++ gaia-demo/src/main/java/github/grit/gaia/demo/leetcode/Solution.java
@@ -3,6 +3,23 @@ package github.grit.gaia.demo.leetcode;
 import java.util.*;
 
 public class Solution {
+    // 812. 最大三角形面积
+    public double largestTriangleArea(int[][] points) {
+        double res = 0;
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                for (int k = j + 1; k < points.length; k++) {
+                    int x1 = points[i][0], y1 = points[i][1];
+                    int x2 = points[j][0], y2 = points[j][1];
+                    int x3 = points[k][0], y3 = points[k][1];
+                    res = Math.max(res, 0.5 * Math.abs(
+                            (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1))
+                    );
+                }
+            }
+        }
+        return res;
+    }
 
     public static void main(String[] args) {
 
