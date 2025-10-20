3a1bb914605fa29cbf1ad2a139930738d5ebe55e
lijunyi <platotomato2002@gmail.com>
2025-10-20 17:48:34 +0800

    feat: 只是积累

---
+++ gaia-work/src/main/java/github/grit/gaia/work/GaiaWorkApplication.java
@@ -0,0 +1,12 @@
+package github.grit.gaia.work;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaiaWorkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GaiaWorkApplication.class, args);
+    }
+} 
